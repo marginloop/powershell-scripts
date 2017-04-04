b5550b0a384af1143959f5d0c7125fd4d1362325
fm-cduffy <cduffy@flexmanage.com>
2017-04-04 11:44:59 -0700

    adding script files from local repo

---
+++ AddUsersToMailboxes.ps1
@@ -0,0 +1,19 @@
+﻿$groups = import-csv C:\Users\cduffy\Documents\WindowsPowerShell\scripts\input\mailboxes.csv
+$users = import-csv C:\Users\cduffy\Documents\WindowsPowerShell\scripts\input\users.csv
+
+$p = Read-Host -AsSecureString "password"
+
+foreach($group in $groups){
+
+     $A = $group.Alias
+
+     foreach($user in $users){
+        $u = Get-User -Filter {Name -like "*$user*"}
+        Add-MailboxPermission -Identity "$A" -User "" -AccessRights FullAccess
+
+     }
+    
+}
+
+
+#Display name, User name, Password, ou, primary smtp address
