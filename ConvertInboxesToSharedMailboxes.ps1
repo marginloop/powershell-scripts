463131152f327d86555864656560d4288a87d781
connor <cduffy@flexmanage.com>
2017-04-05 21:47:49 -0700

    adding working script file

---
+++ ConvertInboxesToSharedMailboxes.ps1
@@ -0,0 +1,17 @@
+﻿#setting up default output and input paths
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+$outputpath="$dir\output\"
+$inputpath="$dir\input\"
+
+$file = import-csv "$inputpath\mailboxes.csv"
+
+foreach($f in $file){
+
+     $DN = $f.DisplayName
+
+     #Set-MsolUserLicense -UserPrincipalName "$upn" -AddLicenses "EXCHANGE_S_ENTERPRISE" | out-file -Append "$output\dlcommands_$(get-date -Format yyyy-MM-dd).txt"
+     Set-Mailbox -Identity $DN -Type Shared -Verbose
+
+}
