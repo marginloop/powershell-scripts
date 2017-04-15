c4d3f40b9093bab0147921048954b4735ba538d5
durffy <cduffy@flexmanage.com>
2017-04-15 08:30:38 -0700

    adding new script o365_SetupAllResourceCalendards.ps1

---
+++ o365_SetupAllResourceCalendars.ps1
@@ -0,0 +1,19 @@
+﻿#setting up default output and input paths
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+$outputpath="$dir\output\"
+$inputpath="$dir\input\"
+
+
+#Main Working Code
+#grab all mailboxes with the type Room, and Equipment
+$mailboxes = Get-Mailbox -RecipientTypeDetails RoomMailbox, EquipmentMailbox
+
+#for each mailbox, 
+foreach($mailbox in $mailboxes){
+
+     $m = $mailbox.DisplayName
+     Set-CalendarProcessing -Identity $m -AutomateProcessing AutoAccept -DeleteSubject $False -AddOrganizerToSubject $False
+     Get-CalendarProcessing -Identity $m | Select Identity, AutomateProcessing, DeleteSubject, AddOrganizerToSubject
+}
+
