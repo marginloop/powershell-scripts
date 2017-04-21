0255286b519b8dbb616a5fdc41b599a2e934f191
durffy <cduffy@flexmanage.com>
2017-04-21 15:48:58 -0700

    time tracking: new time tracking project

---
+++ app_WIP-TimeTracking.ps1
@@ -0,0 +1,47 @@
+﻿$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+$outputpath="$dir\output\"
+$inputpath="$dir\input\"
+
+$outcsv = "$dir\time-tracking_$(get-date -format yyyy-MM-dd).csv"
+
+if(!(Test-Path $Path)){
+     
+    New-Item -Path $outcsv -ItemType directory
+
+}
+
+#main handler
+function Invoke-Time {
+    
+}
+
+#set start_time
+#set end_time
+#check start_time
+#if start_time entry, then write to end_time
+function settime{
+    
+
+}
+
+#get current time from csv
+funtion gettime{
+    $incsv = Import-Csv $outcsv
+}
+
+#assign desctiption id
+#write id in seperate file, with time
+#use id until end_time is filled
+function setdesc{
+    
+}
+
+function getdesc{
+    $incsv = Import-Csv $outcsv
+    $incsv.id
+
+}
+#get all descriptions matching id
+#output to console
+
